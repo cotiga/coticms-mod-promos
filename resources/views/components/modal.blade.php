306804c5e322ee03b2bead36a574ffdd58fa6f78
Serge Ledig <contact@cotiga.fr>
2026-06-21 21:42:35 +0200

    feat: bouton Fermer dans le footer de la modal

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -41,6 +41,9 @@
                         <div class="cotinymce">{!! $promos->first()->contenu !!}</div>
                     @endif
                 </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                </div>
             </div>
         </div>
     </div>
